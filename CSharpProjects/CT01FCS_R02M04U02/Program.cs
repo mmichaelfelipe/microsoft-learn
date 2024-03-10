13efcd1832f0df8c0c29ca20e22d89a46cde83ba
mmichaelfelipe <mmichaelfelipe.ti@gmail.com>
2024-03-10 18:51:30 -0300

    Completed content CT01FCS_R02M04U02

---
+++ CSharpProjects/CT01FCS_R02M04U02/Program.cs
@@ -0,0 +1,113 @@
+﻿// Roteiro 02   - "Criar e executar aplicativos de console C# simples (Introdução ao C#, Parte 2)"
+// Modulo 04    - "Armazenar e iterar em sequências de dados usando Matrizes e a instrução foreach em C#"
+// Unidade 02   - "Exercício – Introdução a matrizes" (19min)
+// https://learn.microsoft.com/pt-br/training/modules/csharp-arrays/2-exercise-array-basics
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region CT01FCS_R02M04U2
+
+
+
+    #region Declarar uma nova matriz
+//string[] fraudulentOrdersIDs = new string[3];
+    #endregion
+
+
+    #region Atribuir valores aos elementos de uma matriz
+// string[] fraudulentOrderIDs = new string[3];
+
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+    #endregion
+
+
+    #region Tentar usar um índice fora dos limites da matriz
+// string[] fraudulentOrderIDs = new string[3];
+
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+// //fraudulentOrderIDs[3] = "D000";
+    #endregion
+
+
+    #region Recuperar valores dos elementos de uma matriz
+// string[] fraudulentOrderIDs = new string[3];
+
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+// //fraudulentOrderIDs[3] = "D000";
+
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+    #endregion
+
+
+    #region Reatribuir o valor de uma matriz
+// string[] fraudulentOrderIDs = new string[3];
+
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+// //fraudulentOrderIDs[3] = "D000";
+
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+// fraudulentOrderIDs[0] = "F000";
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+    #endregion
+
+
+    #region Inicializar uma matriz
+// /*
+// string[] fraudulentOrderIDs = new string[3];
+
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+// //fraudulentOrderIDs[3] = "D000";
+// */
+// string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+// fraudulentOrderIDs[0] = "F000";
+
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+    #endregion
+
+
+    #region Usar a propriedade Length da matriz
+/*
+string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+//fraudulentOrderIDs[3] = "D000";
+*/
+string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process");
+    #endregion
+
+
+
+#endregion
