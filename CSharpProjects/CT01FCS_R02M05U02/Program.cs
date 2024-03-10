f7e48fe64c5a331262439032e6017d1307524f33
mmichaelfelipe <mmichaelfelipe.ti@gmail.com>
2024-03-10 19:08:00 -0300

    Completed content CT01FCS_R02M05U02

---
+++ CSharpProjects/CT01FCS_R02M05U02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Roteiro 02   - "Criar e executar aplicativos de console C# simples (Introdução ao C#, Parte 2)"
+// Modulo 05    - "Criar código legível com convenções, espaço em branco e comentários em C#"
+// Unidade 02   - "Escolher nomes de variáveis que sigam as regras e convenções" (06min)
+// https://learn.microsoft.com/pt-br/training/modules/csharp-readable-code/2-choose-variable-names
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region CT01FCS_R02M05U2
+
+
+
+    #region Exemplos de nome de variável
+char userOption;
+
+int gameScore;
+
+float particlesPerMillion;
+
+bool processedCustomer;
+    #endregion
+
+
+
+#endregion
