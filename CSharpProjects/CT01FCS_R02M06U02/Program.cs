8df2d6ef0dfa0e1fca32b5840650cdbd2fb140bb
mmichaelfelipe <mmichaelfelipe.ti@gmail.com>
2024-03-12 21:34:15 -0300

    Completed content CT01FCS_R02M06U02

---
+++ CSharpProjects/CT01FCS_R02M06U02/Program.cs
@@ -0,0 +1,7 @@
+﻿// Roteiro 02   - "Criar e executar aplicativos de console C# simples (Introdução ao C#, Parte 2)"
+// Modulo 06    - "Projeto guiado – Desenvolver estruturas foreach e if-elseif-else para processar dados de matriz em C#"
+// Unidade 02   - "Preparar" (09min)
+// https://learn.microsoft.com/pt-br/training/modules/guided-project-arrays-iteration-selection/2-prepare
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
