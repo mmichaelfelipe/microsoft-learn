51e0fd45a16f90939692dc5f213158b87a768d5f
Michael Felipe <michaelfelipe.ti@gmail.com>
2024-03-25 07:21:37 -0300

    Content started CT01FCS_R03M01U03
    Signed-off-by: Michael Felipe <michaelfelipe.ti@gmail.com>

---
+++ CSharpProjects/CT01FCS_R03M01U03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Roteiro 03   - "Adicionar lógica a aplicativos de console C# (Introdução ao C#, Parte 3)"
+// Modulo 01    - "Avaliar expressões boolianas para tomar decisões em C#"
+// Unidade 03   - "Exercício – Implementar o operador condicional" (07min)
+// https://learn.microsoft.com/pt-br/training/modules/csharp-evaluate-boolean-expressions/3-exercise-conditional-operator
+
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+#region CT01FCS_R03M01U03
+
+#endregion
