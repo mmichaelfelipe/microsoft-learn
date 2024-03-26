11184f0266db283a86df164a9bb620694e3f7617
mmichaelfelipe <mmichaelfelipe.ti@gmail.com>
2024-03-26 10:41:42 -0300

    Completed content CT01FCS_R03M01U03
    Signed-off-by: mmichaelfelipe <mmichaelfelipe.ti@gmail.com>

--- CSharpProjects/CT01FCS_R03M01U03/Program.cs
+++ CSharpProjects/CT01FCS_R03M01U03/Program.cs
@@ -9,4 +9,38 @@
 
 #region CT01FCS_R03M01U03
 
+
+
+#region O que é o operador condicional?
+/* O operador condicional também é comumente chamado de "operador (condicional) ternário."
+ * Representado da forma        ? :
+ * <evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>
+ */
+#region temp exercicio
+//int valorCompra1 = 999;
+//int valorCompra2 = 1001;
+
+//int valorDescontoCompra1 = (valorCompra1 >= 1000 ? 100 : 50);
+//int valorDescontoCompra2 = (valorCompra2 >= 1000 ? 100 : 50);
+
+//Console.WriteLine($"O valor de desconto da compra 1 foi {valorDescontoCompra1}");
+//Console.WriteLine($"O valor de desconto da compra 2 foi {valorDescontoCompra2}");
+#endregion
+
+#region Adicionar código que usa um operador condicional
+//int saleAmount = 1001;
+//int discount = saleAmount > 1000 ? 100 : 50;
+//Console.WriteLine($"Discount: {discount}");
+#endregion
+
+#region Usar o operador condicional embutido
+int saleAmount = 1001;
+//int discount = saleAmount > 1000 ? 100 : 50;
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+#endregion
+
+#endregion
+
+
+
 #endregion
