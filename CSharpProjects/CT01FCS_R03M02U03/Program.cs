d9722884e6fc67f316f4e534d2704a8f61741404
mmichaelfelipe <mmichaelfelipe.ti@gmail.com>
2024-04-02 10:50:09 -0300

    Completed content CT01FCS_R03M02U03
    Signed-off-by: mmichaelfelipe <mmichaelfelipe.ti@gmail.com>

---
+++ CSharpProjects/CT01FCS_R03M02U03/Program.cs
@@ -0,0 +1,53 @@
+﻿// Roteiro 03   - "Adicionar lógica a aplicativos de console C# (Introdução ao C#, Parte 3)"
+// Modulo 02    - "Controlar o escopo da variável e a lógica usando blocos de código em C#"
+// Unidade 03   - "Exercício – Remover blocos de código de instruções if" (06min)
+// https://learn.microsoft.com/pt-br/training/modules/csharp-code-blocks/3-exercise-remove-if-statements
+
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+#region CT01FCS_R03M02U03
+
+
+
+#region Criar um exemplo de instrução if que usa um bloco de código
+//bool flag = true;
+//if (flag)
+//{
+//    Console.WriteLine(flag);
+//}
+#endregion
+
+#region Remover as chaves
+//bool flag = true;
+//if (flag)
+//    Console.WriteLine(flag);
+#endregion
+
+#region Examinar a legibilidade de instruções if com apenas uma linha
+#region 1.
+//bool flag = true;
+//if (flag) Console.WriteLine(flag);
+#endregion
+#region 5.
+//string name = "steve";
+//if (name == "bob") Console.WriteLine("Found Bob");
+//else if (name == "steve") Console.WriteLine("Found Steve");
+//else Console.WriteLine("Found Chucl");
+#endregion
+#region 7.
+string name = "steve";
+
+if (name == "bob")
+    Console.WriteLine("Found Bob");
+else if (name == "steve")
+    Console.WriteLine("Found Steve");
+else
+    Console.WriteLine("Found Chuck");
+#endregion
+#endregion
+
+
+
+#endregion
