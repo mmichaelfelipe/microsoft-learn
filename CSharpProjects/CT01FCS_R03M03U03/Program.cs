edfb63388bebb56b3b80b1e69294e43d15fd4173
Michael Felipe <michaelfelipe.ti@gmail.com>
2024-04-04 22:35:59 -0300

    Completed and refactoring content CT01FCS_R03M03U03
    Signed-off-by: Michael Felipe <michaelfelipe.ti@gmail.com>

--- CSharpProjects/CT01FCS_R03M03U03/Program.cs
+++ CSharpProjects/CT01FCS_R03M03U03/Program.cs
@@ -7,186 +7,174 @@
 // See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
-namespace CT01FCS_R03M03U03;
+#region CT01FCS_R03M03U03
 
-public static class Program
+
+
+#region Desafio de código: reescrever if-elseif-else usando uma instrução switch
+#region 2. Versão Original do código
+//// SKU = Stock Keeping Unit. 
+//// SKU value format: <product #>-<2-letter color code>-<size code>
+//string sku = "01-MN-L";
+
+//string[] product = sku.Split('-');
+
+//string type = "";
+//string color = "";
+//string size = "";
+
+//if (product[0] == "01")
+//{
+//    type = "Sweat shirt";
+//}
+//else if (product[0] == "02")
+//{
+//    type = "T-Shirt";
+//}
+//else if (product[0] == "03")
+//{
+//    type = "Sweat pants";
+//}
+//else
+//{
+//    type = "Other";
+//}
+
+//if (product[1] == "BL")
+//{
+//    color = "Black";
+//}
+//else if (product[1] == "MN")
+//{
+//    color = "Maroon";
+//}
+//else
+//{
+//    color = "White";
+//}
+
+//if (product[2] == "S")
+//{
+//    size = "Small";
+//}
+//else if (product[2] == "M")
+//{
+//    size = "Medium";
+//}
+//else if (product[2] == "L")
+//{
+//    size = "Large";
+//}
+//else
+//{
+//    size = "One Size Fits All";
+//}
+
+//Console.WriteLine($"Product: {size} {color} {type}");
+#endregion
+
+#region 3. Minha versão de resposta para o desafio
+/* Para esse desafio a solução foi desenvolvida utilizando Switch Expressions, introduzido na linguagem na versão 8.0 do C#
+    * Transformaria ainda os elementod do SKU nos enums Type, Color e Size  para tornar o código autodocumentável.
+    */
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = string.Empty;
+string color = string.Empty;
+string size = string.Empty;
+
+type = product[0] switch
 {
-    public enum SKUType
-    {
-        SweatShirt = 01,
-        TShirt = 02,
-        SweatPants = 03
-    }
+    "01" => "Sweat shirt",
+    "02" => "T-Shirt",
+    "03" => "Sweat pants",
+    _ => "Other"
+};
 
-    public static void Main()
-    {
-        #region CT01FCS_R03M03U03
+color = product[1] switch
+{
+    "BL" => "Black",
+    "MN" => "Maroon",
+    _ => "White"
+};
 
+size = product[2] switch
+{
+    "S" => "Small",
+    "M" => "Medium",
+    "L" => "Large",
+    _ => "One Size Fits All"
+};
 
+Console.WriteLine($"Product: {size} {color} {type}");
+#endregion
 
-        #region Desafio de código: reescrever if-elseif-else usando uma instrução switch
-        #region 2. Versão Original do código
-        //// SKU = Stock Keeping Unit. 
-        //// SKU value format: <product #>-<2-letter color code>-<size code>
-        //string sku = "01-MN-L";
+#region Refatorando minha versão de resposta para o desafio
+/* Código contido na classe Refactoring.cs dessa solução.
+    * A ideia é a utilização de Enum para facilitar a legibilidade do mapeamento dos padrões do SKU.
+    */
+#endregion
+#endregion
 
-        //string[] product = sku.Split('-');
+#region Resposta oficial
+//// SKU = Stock Keeping Unit
+//string sku = "01-MN-L";
 
-        //string type = "";
-        //string color = "";
-        //string size = "";
+//string[] product = sku.Split('-');
 
-        //if (product[0] == "01")
-        //{
-        //    type = "Sweat shirt";
-        //}
-        //else if (product[0] == "02")
-        //{
-        //    type = "T-Shirt";
-        //}
-        //else if (product[0] == "03")
-        //{
-        //    type = "Sweat pants";
-        //}
-        //else
-        //{
-        //    type = "Other";
-        //}
+//string type = "";
+//string color = "";
+//string size = "";
 
-        //if (product[1] == "BL")
-        //{
-        //    color = "Black";
-        //}
-        //else if (product[1] == "MN")
-        //{
-        //    color = "Maroon";
-        //}
-        //else
-        //{
-        //    color = "White";
-        //}
+//switch (product[0])
+//{
+//    case "01":
+//        type = "Sweat shirt";
+//        break;
+//    case "02":
+//        type = "T-Shirt";
+//        break;
+//    case "03":
+//        type = "Sweat pants";
+//        break;
+//    default:
+//        type = "Other";
+//        break;
+//}
 
-        //if (product[2] == "S")
-        //{
-        //    size = "Small";
-        //}
-        //else if (product[2] == "M")
-        //{
-        //    size = "Medium";
-        //}
-        //else if (product[2] == "L")
-        //{
-        //    size = "Large";
-        //}
-        //else
-        //{
-        //    size = "One Size Fits All";
-        //}
+//switch (product[1])
+//{
+//    case "BL":
+//        color = "Black";
+//        break;
+//    case "MN":
+//        color = "Maroon";
+//        break;
+//    default:
+//        color = "White";
+//        break;
+//}
 
-        //Console.WriteLine($"Product: {size} {color} {type}");
-        #endregion
+//switch (product[2])
+//{
+//    case "S":
+//        size = "Small";
+//        break;
+//    case "M":
+//        size = "Medium";
+//        break;
+//    case "L":
+//        size = "Large";
+//        break;
+//    default:
+//        size = "One Size Fits All";
+//        break;
+//}
 
-        #region 3. Minha versão de resposta para o desafio
-        /* Para esse desafio a solução foi desenvolvida utilizando Switch Expressions, introduzido na linguagem na versão 8.0 do C#
-            * Transformaria ainda os elementod do SKU nos enums Type, Color e Size  para tornar o código autodocumentável.
-            */
-        // SKU = Stock Keeping Unit. 
-        // SKU value format: <product #>-<2-letter color code>-<size code>
-        string sku = "01-MN-L";
-
-        string[] product = sku.Split('-');
-
-        string type = string.Empty;
-        string color = string.Empty;
-        string size = string.Empty;
-
-        type = product[0] switch
-        {
-            "01" => "Sweat shirt",
-            "02" => "T-Shirt",
-            "03" => "Sweat pants",
-            _ => "Other"
-        };
-
-        color = product[1] switch
-        {
-            "BL" => "Black",
-            "MN" => "Maroon",
-            _ => "White"
-        };
-
-        size = product[2] switch
-        {
-            "S" => "Small",
-            "M" => "Medium",
-            "L" => "Large",
-            _ => "One Size Fits All"
-        };
-
-        Console.WriteLine($"Product: {size} {color} {type}");
-        #endregion
-        #endregion
-
-        #region Resposta oficial
-        //// SKU = Stock Keeping Unit
-        //string sku = "01-MN-L";
-
-        //string[] product = sku.Split('-');
-
-        //string type = "";
-        //string color = "";
-        //string size = "";
-
-        //switch (product[0])
-        //{
-        //    case "01":
-        //        type = "Sweat shirt";
-        //        break;
-        //    case "02":
-        //        type = "T-Shirt";
-        //        break;
-        //    case "03":
-        //        type = "Sweat pants";
-        //        break;
-        //    default:
-        //        type = "Other";
-        //        break;
-        //}
-
-        //switch (product[1])
-        //{
-        //    case "BL":
-        //        color = "Black";
-        //        break;
-        //    case "MN":
-        //        color = "Maroon";
-        //        break;
-        //    default:
-        //        color = "White";
-        //        break;
-        //}
-
-        //switch (product[2])
-        //{
-        //    case "S":
-        //        size = "Small";
-        //        break;
-        //    case "M":
-        //        size = "Medium";
-        //        break;
-        //    case "L":
-        //        size = "Large";
-        //        break;
-        //    default:
-        //        size = "One Size Fits All";
-        //        break;
-        //}
-
-        //Console.WriteLine($"Product: {size} {color} {type}");
-        #endregion
-
-
-
-        #endregion
-    }
-}
+//Console.WriteLine($"Product: {size} {color} {type}");
+#endregion
+#endregion
---
+++ CSharpProjects/CT01FCS_R03M03U03/Refactoring.cs
@@ -0,0 +1,83 @@
+﻿// Roteiro 03   - "Adicionar lógica a aplicativos de console C# (Introdução ao C#, Parte 3)"
+// Modulo 03    - "Ramificar o fluxo de código usando o constructo de mudança de casos em C#"
+// Unidade 03   - "Exercício – Concluir uma atividade de desafio usando instruções switch" (09min)
+// https://learn.microsoft.com/pt-br/training/modules/csharp-switch-case/3-challenge
+
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+namespace CT01FCS_R03M03U03;
+
+#region Refatorando minha versão de resposta para o desafio
+public class Refactoring
+{
+    static void Main(string[] args)
+    {
+        var productInfo = new ProductInfo("01-MN-L");
+        productInfo.PrintInfo();
+    }
+}
+
+public enum ProductType
+{
+    SweatShirt,
+    TShirt,
+    SweatPants,
+    Other
+}
+
+public enum Color
+{
+    Black,
+    Maroon,
+    White
+}
+
+public enum Size
+{
+    Small,
+    Medium,
+    Large,
+    OneSizeFitsAll
+}
+
+public class ProductInfo
+{
+    public string Sku { get; }
+    public ProductType Type { get; }
+    public Color Color { get; }
+    public Size Size { get; }
+
+    public ProductInfo(string sku)
+    {
+        Sku = sku;
+        var parts = sku.Split('-');
+        Type = parts[0] switch
+        {
+            "01" => ProductType.SweatShirt,
+            "02" => ProductType.TShirt,
+            "03" => ProductType.SweatPants,
+            _ => ProductType.Other
+        };
+        Color = parts[1] switch
+        {
+            "BL" => Color.Black,
+            "MN" => Color.Maroon,
+            _ => Color.White
+        };
+        Size = parts[2] switch
+        {
+            "S" => Size.Small,
+            "M" => Size.Medium,
+            "L" => Size.Large,
+            _ => Size.OneSizeFitsAll
+        };
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Product: {Size} {Color} {Type}");
+    }
+}
+#endregion
