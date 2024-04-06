2d724e92f1fd5e0e4cf24e696b65a74c5dc6beac
mmichaelfelipe <mmichaelfelipe.ti@gmail.com>
2024-04-06 11:22:18 -0300

    Adjusting block comments in content CT01FCS_R03M05U03
    Signed-off-by: mmichaelfelipe <mmichaelfelipe.ti@gmail.com>

--- CSharpProjects/CT01FCS_R03M05U03/Program.cs
+++ CSharpProjects/CT01FCS_R03M05U03/Program.cs
@@ -11,39 +11,39 @@
 
 #region Desafio da batalha em RPG
 /* Em algumas das funções que desempenham jogos, o personagem do jogador 
- * batalha contra personagens não controlados por outros jogadores, que 
- * geralmente são monstros ou vilões. Às vezes, uma batalha consiste em cada 
- * personagem gerar um valor aleatório jogando dados, e esse valor é subtraído dos 
- * pontos de vida do adversário. Quando a vida de um personagem chega a zero, ele perde o jogo.
+    batalha contra personagens não controlados por outros jogadores, que 
+    geralmente são monstros ou vilões. Às vezes, uma batalha consiste em cada 
+    personagem gerar um valor aleatório jogando dados, e esse valor é subtraído dos 
+    pontos de vida do adversário. Quando a vida de um personagem chega a zero, ele perde o jogo.
 
- * Neste desafio, vamos reduzir essa interação à sua essência. Um herói e um monstro 
- * começam com a mesma pontuação de integridade. Durante a rodada do 
- * herói, ele gerará um valor aleatório que será subtraído da integridade do monstro. 
- * Se a integridade do monstro for maior que zero, ela terá sua rodada e atacará o 
- * herói. Desde que tanto o herói quanto o monstro tenham integridade maior que zero, 
- * a batalha continuará.
+    Neste desafio, vamos reduzir essa interação à sua essência. Um herói e um monstro 
+    começam com a mesma pontuação de integridade. Durante a rodada do 
+    herói, ele gerará um valor aleatório que será subtraído da integridade do monstro. 
+    Se a integridade do monstro for maior que zero, ela terá sua rodada e atacará o 
+    herói. Desde que tanto o herói quanto o monstro tenham integridade maior que zero, 
+    a batalha continuará.
  */
 #endregion
 
 #region Desafio de código - Escrever um código para implementar as regras do jogo
 /* Veja as regras para o jogo de batalha que você precisa implementar em seu projeto de código:
- * - Você deve usar a instrução do-while ou a instrução while como um loop de jogo externo
- * - O herói e o monstro começarão com dez pontos de integridade.
- * - Todos os ataques serão um valor entre 1 e 10.
- * - O herói atacará primeiro.
- * - Imprima a quantidade de integridade que o monstro perdeu e a integridade que resta a ele.
- * - Se a integridade do monstro for maior que zero, ele poderá atacar o herói.
- * - Imprima a quantidade de integridade que o herói perdeu e a integridade que resta a ele.
- * - Continue esta sequência de ataque até que a integridade do monstro ou do herói seja zero ou menos.
- * - Imprima quem foi o vencedor.
- * 
- * Não importa como você fará isso; o código deverá produzir uma saída semelhante:
- * Monster was damaged and lost 1 health and now has 9 health
- * Hero was damaged and lost 1 health and now has 9 health
- * Monster was damaged and lost 7 health and now has 2 health
- * Hero was damaged and lost 6 health and now has 3 health
- * Monster was damaged and lost 9 health and now has -7 health
- * Hero wins!
+    - Você deve usar a instrução do-while ou a instrução while como um loop de jogo externo
+    - O herói e o monstro começarão com dez pontos de integridade.
+    - Todos os ataques serão um valor entre 1 e 10.
+    - O herói atacará primeiro.
+    - Imprima a quantidade de integridade que o monstro perdeu e a integridade que resta a ele.
+    - Se a integridade do monstro for maior que zero, ele poderá atacar o herói.
+    - Imprima a quantidade de integridade que o herói perdeu e a integridade que resta a ele.
+    - Continue esta sequência de ataque até que a integridade do monstro ou do herói seja zero ou menos.
+    - Imprima quem foi o vencedor.
+    
+    Não importa como você fará isso; o código deverá produzir uma saída semelhante:
+    Monster was damaged and lost 1 health and now has 9 health
+    Hero was damaged and lost 1 health and now has 9 health
+    Monster was damaged and lost 7 health and now has 2 health
+    Hero was damaged and lost 6 health and now has 3 health
+    Monster was damaged and lost 9 health and now has -7 health
+    Hero wins!
  */
 
 #region Minha versão do desafio
