462507ebb7e7ac8c475a1c0c5802993530c2001c
mmichaelfelipe <michaelfelipe.ti@gmail.com>
2024-10-18 16:55:40 -0300

    Completed content CT01FCS_R04M03U02
    Signed-off-by: mmichaelfelipe <michaelfelipe.ti@gmail.com>

---
+++ CSharpProjects/CT01FCS_R04M03U02/Program.cs
@@ -0,0 +1,47 @@
+﻿// Roteiro 04   - "Trabalhar com os dados variáveis em aplicativos de console C# (Introdução ao C#, Parte 4)"
+// Modulo 02    - "Executar operações em matrizes usando métodos auxiliares em C#"
+// Unidade 02   - "Exercício – Conhecer as funções Sort() e Reverse()" (10min)
+// https://learn.microsoft.com/pt-br/training/modules/csharp-arrays-operations/2-exercise-sort-reverse
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+#region CT01FCS_R04M03U02
+
+
+#region Criar uma matriz de paletes, depois classificá-los
+#region 2.
+//string[] pallets = { "B14", "A11", "B12", "A13" };
+
+//Console.WriteLine("Sorted...");
+//Array.Sort(pallets);
+//foreach (var pallet in pallets)
+//{
+//    Console.WriteLine($"-- {pallet}");
+//}
+#endregion
+#endregion
+
+
+#region Inverter a ordem dos paletes
+#region 1.
+string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Console.WriteLine("Reversed...");
+Array.Reverse(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+#endregion
+#endregion
+
+#endregion
