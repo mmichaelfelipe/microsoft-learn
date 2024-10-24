4985b6f2a56141f6de621a183c104be63bc6899e
mmichaelfelipe <michaelfelipe.ti@gmail.com>
2024-10-24 09:07:52 -0300

    Completed content CT01FCS_R05M04U02

---
+++ CSharpProjects/CT01FCS_R05M04U02/Program.cs
@@ -0,0 +1,26 @@
+﻿// Roteiro 05   - "Criar métodos em aplicativos de console C# (Introdução ao C#, Parte 5)"
+// Modulo 04    - "Projeto guiado – Planejar uma visita ao zoológico"
+// Unidade 02   - "Preparar-se para o projeto guiado" (03min)
+// https://learn.microsoft.com/pt-br/training/modules/guided-project-visit-petting-zoo/2-prepare
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+#region CT01FCS_R01M04U02
+
+#region Instalação
+#region Preparar o ambiente de codificação
+#region 11.
+using System;
+
+string[] pettingZoo =
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+#endregion
+#endregion
+#endregion
+
+#endregion
