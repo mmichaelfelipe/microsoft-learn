1917877e03d741b719c2e18c257f261253c8ae93
mmichaelfelipe <michaelfelipe.ti@gmail.com>
2024-10-24 09:49:16 -0300

    Completed content CT01FCS_R05M04U06

---
+++ CSharpProjects/CT01FCS_R05M04U06/Program.cs
@@ -0,0 +1,80 @@
+﻿// Roteiro 05   - "Criar métodos em aplicativos de console C# (Introdução ao C#, Parte 5)"
+// Modulo 04    - "Projeto guiado – Planejar uma visita ao zoológico"
+// Unidade 06   - "Exercício – Criar um método para exibir os resultados" (03min)
+// https://learn.microsoft.com/pt-br/training/modules/guided-project-visit-petting-zoo/6-exercise-display-results
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+#region CT01FCS_R01M04U06
+
+
+#region Desenvolver seu método
+#region 1., 2., 3., 7., 8.
+using System;
+
+string[] pettingZoo =
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+PlanSchoolVisit("School A");
+PlanSchoolVisit("School B", 3);
+PlanSchoolVisit("School C", 2);
+
+void PlanSchoolVisit(string schoolName, int groups = 6)
+{
+    RandomizeAnimals();
+    string[,] group = AssignGroup(groups);
+    Console.WriteLine(schoolName);
+    PrintGroup(group);
+}
+
+void RandomizeAnimals()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++)
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[i];
+        pettingZoo[i] = pettingZoo[r];
+        pettingZoo[r] = temp;
+    }
+}
+
+string[,] AssignGroup(int groups = 6)
+{
+    string[,] result = new string[groups, pettingZoo.Length / groups];
+    int start = 0;
+
+    for (int i = 0; i < groups; i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
+}
+
+void PrintGroup(string[,] group)
+{
+    for (int i = 0; i < group.GetLength(0); i++)
+    {
+        Console.Write($"Group { i + 1}: ");
+        for (int j = 0; j < group.GetLength(1); j++)
+        {
+            Console.Write($"{group[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+#endregion
+#endregion
+
+#endregion
