350e5e5c6b2b1599ee0bfb644b62b86eea2cba8b
mmichaelfelipe <michaelfelipe.ti@gmail.com>
2024-10-26 18:08:14 -0300

    Completed content CT01FCS_R06M05U02

---
+++ CSharpProjects/CT01FCS_R06M05U02/Program.cs
@@ -0,0 +1,11 @@
+﻿// Roteiro 06   - "Depurar aplicativos de console do C# (Introdução ao C#, Parte 6)"
+// Modulo 05    - "Projeto guiado – Depurar e manipular exceções em um aplicativo de console C# usando o Visual Studio Code"
+// Unidade 02   - "Preparar" (07min)
+// https://learn.microsoft.com/pt-br/training/modules/guided-project-debug-handle-exceptions-c-sharp-console-application/2-prepare
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+#region CT01FCS_R06M05U02
+Console.WriteLine("Hello, World!");
+#endregion
